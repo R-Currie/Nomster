6afb87d84fb6899223967ddbab84706451882ce8
Robert Currie <currierobertm@gmail.com>
2019-07-31 19:29:55 +0000

    Edit permissions and fixed typo

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit. :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
 
   def index
     @places = Place.all.paginate(page: params[:page], per_page: 5)
